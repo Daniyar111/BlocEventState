80d40acbf4f4794a03971a502fe1f26def20cd09
Daniyar111 <dani.changylov@gmail.com>
2019-07-12 22:35:13 -0700

    base bloc provider added

---
+++ lib/src/data/providers/bloc_provider.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+Type _typeOf<T>() => T;
+
+abstract class BlocBase{
+  void dispose();
+}
+
+
+class BlocProvider<T extends BlocBase> extends StatefulWidget {
+
+  final Widget child;
+  final T bloc;
+
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc
+  }) : super(key: key);
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BlocBase>(BuildContext context){
+    final type = _typeOf<_BlocProviderInherited<T>>();
+    _BlocProviderInherited<T> provider = context.ancestorInheritedElementForWidgetOfExactType(type)?.widget;
+    return provider?.bloc;
+  }
+}
+
+class _BlocProviderState<T extends BlocBase> extends State<BlocProvider<T>> {
+
+  @override
+  Widget build(BuildContext context) {
+    return _BlocProviderInherited<T>(
+      bloc: widget.bloc,
+      child: widget.child,
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.bloc?.dispose();
+    super.dispose();
+  }
+}
+
+
+class _BlocProviderInherited<T> extends InheritedWidget {
+
+  final T bloc;
+
+  const _BlocProviderInherited({
+    Key key,
+    @required Widget child,
+    @required this.bloc
+  }) : assert(child != null),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_BlocProviderInherited old) {
+    return false;
+  }
+}
