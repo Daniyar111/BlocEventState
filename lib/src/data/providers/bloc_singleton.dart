1a58a668bae61150c48213a2bb524e7171dfdeaf
Daniyar111 <dani.changylov@gmail.com>
2019-07-15 22:08:20 -0700

    global bloc

---
+++ lib/src/data/providers/bloc_singleton.dart
@@ -0,0 +1,23 @@
+import 'package:rxdart/rxdart.dart';
+
+GlobalBloc globalBloc = GlobalBloc();
+
+class GlobalBloc{
+
+  // Streams related to this bloc
+  BehaviorSubject<String> _controller = BehaviorSubject<String>();
+  Function(String) get push => _controller.sink.add;
+  Stream<String> get stream => _controller;
+
+  // Singleton factory
+  GlobalBloc._internal();
+  static final GlobalBloc _bloc = GlobalBloc._internal();
+  factory GlobalBloc(){
+    return _bloc;
+  }
+
+  // Resource disposal
+  void dispose(){
+    _controller.close();
+  }
+}
