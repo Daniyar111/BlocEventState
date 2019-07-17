7e08d838fe9d3f2b8a941857b86eda3f2488e2bc
Daniyar111 <dani.changylov@gmail.com>
2019-07-17 05:26:25 -0700

    home page added

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  // Prevents the use of the 'back' button
+  Future<bool> _onWillPopScope() async {
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPopScope,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Home Page'),
+            leading: Container(),
+            actions: <Widget>[
+              LogOutButton()
+            ],
+          ),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: RaisedButton(
+                    child: Text('Shopping'),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => ShoppingPage()
+                        )
+                      );
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: RaisedButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Shopping Basket'),
+                        SizedBox(width: 16.0,),
+                        ShoppingBasket(),
+                        SizedBox(width: 16.0,),
+                        ShoppingBasketPrice()
+                      ],
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).pushNamed('/shoppingBasket');
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
